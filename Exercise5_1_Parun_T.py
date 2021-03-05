9652d466590464c6bd1f0d84c12ec517e406f254
Neilos_ <parun.bcc37884@gmail.com>
2021-03-06 00:05:52 +0700

    Create Exercise5_1_Parun_T.py

---
+++ Exercise5_1_Parun_T.py
@@ -0,0 +1,12 @@
+p1V = int(input("Please Enter 1st Value:"))
+p2V = int(input("Please Enter 2st Value:"))
+result1 = p1V + p2V
+result2 = p1V - p2V
+result3 = p1V * p2V
+result4 = p1V / p2V
+print ("----------Result--------")
+print (result1)
+print (result2)
+print (result3)
+print (result4)
+print ("----------Result--------")
